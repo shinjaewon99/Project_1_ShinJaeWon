5463762b69ee27b35bf2e974920e7016c6fb9a6a
shinjaewon99 <tlswodnjs99@naver.com>
2023-07-28 15:17:37 +0900

    feat : security 설정 파일 추가 #12

---
+++ project_1/src/main/java/com/example/mini_project/config/WebSecurityConfig.java
@@ -0,0 +1,48 @@
+package com.example.mini_project.config;
+
+import com.example.mini_project.util.jwt.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration // 설정파일이라는것을 알려주는 어노테이션
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class WebSecurityConfig {
+    private final JwtAuthenticationFilter jwtAuthFilter;
+    private final AuthenticationProvider authenticationProvider;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                /**
+                 csrf  비활성화 : non-browser clients만을 위한 서비스라면 csrf를 비활성화 해도 좋다.
+                 cors 비활성화 : 다른곳의 AJAX 요청을 허용합니다. (필요한 경우에만 비활성화 한다.)
+                 사용 : .cors(AbstractHttpConfigurer::disable)
+                 **/
+                .csrf()
+                .disable()
+                // 요청에 대한 보안 설정
+                .authorizeHttpRequests()
+                .requestMatchers("")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                // 세션관리 설정
+                // STATELESS 즉 무상태로 설정하는 것이 일반적으로 jwt 인증을 사용하는데 사용
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+}
