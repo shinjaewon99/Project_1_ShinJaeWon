17525cca27e8cbbcbdaebbebe29800587d87225d
shinjaewon99 <tlswodnjs99@naver.com>
2023-07-29 00:01:10 +0900

    refactor : 회원가입 반환 타입 수정 #23

--- project_1/src/main/java/com/example/mini_project/service/user/UserService.java
+++ project_1/src/main/java/com/example/mini_project/service/user/UserService.java
@@ -1,10 +1,11 @@
 package com.example.mini_project.service.user;
 
 import com.example.mini_project.auth.AuthenticationResponse;
+import com.example.mini_project.dto.ResponseDto;
 import com.example.mini_project.dto.user.Request.LoginRequestDto;
 import com.example.mini_project.dto.user.Request.RegisterRequest;
 
 public interface UserService {
-    AuthenticationResponse register(RegisterRequest request);
+    ResponseDto register(RegisterRequest request);
     AuthenticationResponse authenticate(LoginRequestDto request);
 }
