1e91692ae0a48d5f80157344dc4eb2d02043439f
shinjaewon99 <tlswodnjs99@naver.com>
2023-07-28 23:59:47 +0900

    feat : jwt 인증 필터 클래스 추가 #22

---
+++ project_1/src/main/java/com/example/mini_project/util/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,63 @@
+package com.example.mini_project.util.jwt;
+
+import com.example.mini_project.config.JwtService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.constraints.NotNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+@Component // @Component : 개발자가 직접 컨트롤이 가능한 클래스의 경우 사용, @Baen은 메소드에 사용
+// OncePerRequestFilter : 한번의 요청에 대해 한번만 실행하는 필터
+// 모든 요청과 응답을 가로채어 처리합니다.
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(
+            @NotNull HttpServletRequest request,
+            @NotNull HttpServletResponse response,
+            @NotNull FilterChain filterChain)
+            throws ServletException, IOException {
+        final String authHeader = request.getHeader("Authorization"); // 헤더의 이름이 인증인것
+        final String jwt;
+        final String userId;
+
+        // jwt는 항상 "Bearer " 키워드로 시작한다. (띄어쓰기 유의)
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        jwt = authHeader.substring(7);
+        userId = jwtService.extractUserId(jwt);
+
+        // ~getAuthentication() == null : 사용자가 아직 연결되지 않았음을 의미
+        if (userId != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = this.userDetailsService.loadUserByUsername(userId);
+
+            if (jwtService.isTokenValid(jwt, userDetails)) {
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                        userDetails,
+                        null,
+                        userDetails.getAuthorities()
+                );
+
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+}
