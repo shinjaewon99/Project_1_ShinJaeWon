a8607fbcc4d3dc5a604cd9d36364dc819ab7d1d4
shinjaewon99 <tlswodnjs99@naver.com>
2023-07-31 15:12:16 +0900

    feat : 추후 필요성이 있을때 추가할 Base 엔티티 생성시간, 수정시간 #74

--- project_1/src/main/java/com/example/mini_project/entity/base/BaseEntity.java
+++ project_1/src/main/java/com/example/mini_project/entity/base/BaseEntity.java
@@ -2,17 +2,23 @@ package com.example.mini_project.entity.base;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-/*
-@MappedSuperclass // 엔티티 별로 공통 필드가 존재하는 경우 불 필요한 중복 코드를 제거하기 위해 사용한다.
-@EntityListeners(value = AuditingEntityListener.class) // DB적용 전 공통부분을 등록하기 위해 등록한다.
-@Getter
-// 해당 클래스를 단독으로 사용할 일이 없기때문에 추상클래스로 선언한다.
-public abstract class BaseEntity {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+import java.time.LocalDateTime;
 
-}
-*/
+//@MappedSuperclass // 엔티티 별로 공통 필드가 존재하는 경우 불 필요한 중복 코드를 제거하기 위해 사용한다.
+//@EntityListeners(value = AuditingEntityListener.class) // DB적용 전 공통부분을 등록하기 위해 등록한다.
+//@Getter
+//// 해당 클래스를 단독으로 사용할 일이 없기때문에 추상클래스로 선언한다.
+//public abstract class BaseEntity {
+//    @CreatedDate
+//    @Column(updatable = false)
+//    private LocalDateTime createdAt;
+//
+//    @LastModifiedDate
+//    @Column(updatable = false)
+//    private LocalDateTime updatedAt;
+//
+//}
